53b5c065867a7778ddbce6ad1be568e351a3aaed
Kamiel Verhelst <kamiel.verhelst@geodan.nl>
2023-06-08 12:10:35 +0200

    Creating virtual raster

--- scripts/1_create_tiles.sh
+++ scripts/1_create_tiles.sh
@@ -1,26 +1,26 @@
 echo $PWD
-DIR=tiles
+TILE_DIR=tiles
+DTM_DIR=dtms
 
-if [ ! -d "${DIR}" ];
+if [ ! -d "${TILE_DIR}" ];
 then
-    mkdir -p "${DIR}"
-    echo "${DIR} directory created."
+    mkdir -p "${TILE_DIR}"
+    echo "${TILE_DIR} directory created."
 fi
 
-files=""
-for f in $(find -name '*.tif'); do
-   f="${f%.*}"
-   echo "Processing file ${f}.tif..."
-   gdal_fillnodata.bat ${f}.tif ${f}_filled.tif
-   gdalwarp -s_srs EPSG:7415 -t_srs EPSG:4326+4979 ${f}_filled.tif ${f}_filled_4326.tif
-   files="${files} ${f}_filled_4326.tif"
-done
+echo "Building virtual raster ${DTM_DIR}/ahn.vrt..."
+gdalbuildvrt ${DTM_DIR}/ahn.vrt ${DTM_DIR}/*.TIF
 
-echo "files $files"
-# todo use a vrt instead of merging?
-gdal_merge.bat -o test.tif ${files}
+# Fill nodata in tifs
+echo "Filling nodata values..."
+gdal_fillnodata.bat ${DTM_DIR}/ahn.vrt ${DTM_DIR}/ahn_filled.vrt
+
+# Reproject to 4326 with vertical datum 4979 and save as tif
+echo "Reprojecting to 4326 with vertical datum 4979 and saving as ahn_filled_4326.tif..."
+gdalwarp -s_srs EPSG:7415 -t_srs EPSG:4326+4979 ${DTM_DIR}/ahn_filled.vrt ahn_filled_4326.tif
 
 # create quantized mesh tiles using docker image tumgis/ctb-quantized-mesh
 # todo: use $pwd on Linux
-docker run -it -v D:/dev/github.com/geodan/terrain/scripts:/data tumgis/ctb-quantized-mesh ctb-tile -f Mesh -C -N -e 0 -s 15 -o /data/tiles /data/test.tif
-docker run -it -v D:/dev/github.com/geodan/terrain/scripts:/data tumgis/ctb-quantized-mesh ctb-tile -f Mesh -C -N -e 0 -s 15 -l -o /data/tiles /data/test.tif
+echo "Running ctb-tile in Docker image..."
+docker run -it -v D:/dev/github.com/geodan/terrain/scripts:/data tumgis/ctb-quantized-mesh ctb-tile -f Mesh -C -N -e 0 -s 15 -o /data/tiles /data/ahn_filled_4326.tif
+docker run -it -v D:/dev/github.com/geodan/terrain/scripts:/data tumgis/ctb-quantized-mesh ctb-tile -f Mesh -C -N -e 0 -s 15 -l -o /data/tiles /data/ahn_filled_4326.tif
