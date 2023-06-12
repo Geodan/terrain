0a7a309eb4958a45eb99193132c9eec56e57666b
Bert Temme <btemme@hotmail.com>
2023-06-12 15:43:21 +0200

    first do warp+fill_nodata per file, after that

--- scripts/1_create_tiles.sh
+++ scripts/1_create_tiles.sh
@@ -1,19 +1,22 @@
+#!/bin/sh
 # Set default values 
 input_dir=dtms
 output_dir=tiles
+tmp_dir=tmp
 start_zoom=15
 end_zoom=0
 tif_extension=tif
+s_srs=EPSG:7415
 
 print_usage()
 {
    # Display Help
    echo Syntax: sh 1_create_tiles.sh '[-s|e|h|i|o]'
    echo options:
-   echo i     Input directory - default ${i}
-   echo o     Output directory - default ${o}
-   echo s     Start zoomlevel - default ${start_zoom}
-   echo e     End zoomlevel - default ${end_zoom}
+   echo i     Input directory - default $input_dir
+   echo o     Output directory - default $output_dir
+   echo s     Start zoomlevel - default $start_zoom
+   echo e     End zoomlevel - default $end_zoom
    echo h     Print this help
    echo
 }
@@ -25,47 +28,60 @@ echo
 while getopts i:o:s:e:h flag
 do
     case $flag in
-        i) input_dir=${OPTARG};;
-        o) output_dir=${OPTARG};;
-        s) start_zoom=${OPTARG};;
-        e) end_zoom=${OPTARG};;
+        i) input_dir=$OPTARG;;
+        o) output_dir=$OPTARG;;
+        s) start_zoom=$OPTARG;;
+        e) end_zoom=$OPTARG;;
         h) print_usage; exit 0;;
     esac
 done
 
-echo Input directory: ${input_dir}
-echo Output directory: ${output_dir}
-echo Tif extensions: ${tif_extension}
-echo Start zoomlevel: ${start_zoom}
-echo End zoomlevel: ${end_zoom}
+echo Input directory: $input_dir
+echo Output directory: $output_dir
+echo Tif extension: $tif_extension
+echo Start zoomlevel: $start_zoom
+echo End zoomlevel: $end_zoom
+echo Source SRS: $s_srs
 
-# Create tile directory
-if [ ! -d "${output_dir}" ];
+# Create tmp directory
+if [ ! -d "$tmp_dir" ];
 then
-    mkdir -p "${output_dir}"
-    echo ${output_dir} directory created.
+    mkdir -p "$tmp_dir"
+    echo $tmp_dir directory created.
 fi
 
-# Check if input directory exists and has .tif files
-if ! compgen -G "${input_dir}/*${tif_extension}" > /dev/null; 
+# Create output directory
+if [ ! -d "$output_dir" ];
 then
-    echo Folder ${input_dir} does not exist or does not contain ${tif_extension} files.
+    mkdir -p "$output_dir"
+    echo $output_dir directory created.
+fi
+
+
+# Check if input directory exists and has .tif files
+if ! compgen -G "$input_dir/*$tif_extension" > /dev/null; 
+then
+    echo Folder $input_dir does not exist or does not contain $tif_extension files.
     exit 1
 fi
 
-echo Building virtual raster ${input_dir}/ahn.vrt...
-gdalbuildvrt ${input_dir}/ahn.vrt ${input_dir}/*.${tif_extension}
+for f in $(find -name *.$tif_extension); do
+    echo "Processing file $f..."
+    f_out=$(basename $f)  
+    filename="${f_out%.*}"
 
-# Fill nodata in tifs
-echo Filling nodata values...
-gdal_fillnodata.bat ${input_dir}/ahn.vrt ${input_dir}/ahn_filled.vrt
+    gdal_fillnodata.bat $f $tmp_dir/${filename}_filled.$tif_extension
+    gdalwarp -s_srs $s_srs -t_srs EPSG:4326+4979 $tmp_dir/${filename}_filled.${tif_extension} $tmp_dir/${filename}_filled_4326.${tif_extension}
+    rm $tmp_dir/${filename}_filled.$tif_extension
+done
 
-# Reproject to 4326 with vertical datum 4979 and save as tif
-echo Reprojecting to 4326 with vertical datum 4979 and saving as ${input_dir}ahn_filled_4326.${tif_extension}...
-gdalwarp -s_srs EPSG:7415 -t_srs EPSG:4326+4979 ${input_dir}/ahn_filled.vrt ${input_dir}ahn_filled_4326.${tif_extension}
+echo Building virtual raster ${tmp_dir}/ahn.vrt...
+gdalbuildvrt -overwrite ${tmp_dir}/ahn.vrt ${tmp_dir}/*.$tif_extension 
 
 # create quantized mesh tiles using docker image tumgis/ctb-quantized-mesh
 # todo: use $pwd on Linux
 echo Running ctb-tile in Docker image...
-docker run -it -v D:/dev/github.com/geodan/terrain/scripts:/data tumgis/ctb-quantized-mesh ctb-tile -f Mesh -C -N -e ${end_zoom} -s ${start_zoom} -o /data/tiles /data/ahn_filled_4326.${tif_extension}
-docker run -it -v D:/dev/github.com/geodan/terrain/scripts:/data tumgis/ctb-quantized-mesh ctb-tile -f Mesh -C -N -e ${end_zoom} -s ${start_zoom} -l -o /data/tiles /data/ahn_filled_4326.${tif_extension}
+docker run -it -v D:/dev/github.com/geodan/terrain/scripts:/data tumgis/ctb-quantized-mesh ctb-tile -f Mesh -C -N -e ${end_zoom} -s ${start_zoom} -o ${output_dir} ${tmp_dir}/ahn.vrt
+docker run -it -v D:/dev/github.com/geodan/terrain/scripts:/data tumgis/ctb-quantized-mesh ctb-tile -f Mesh -C -N -e ${end_zoom} -s ${start_zoom} -l -o ${output_dir} ${tmp_dir}/ahn.vrt
+
+rm $tmp_dir/ahn.vrt
