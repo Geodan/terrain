e1fe57a8a712909138f8febdbae1fdbfe15f58a9
Kamiel Verhelst <kamiel.verhelst@geodan.nl>
2023-06-14 10:48:23 +0200

    Completed ahn4 download script

--- scripts/0_download_ahn4.py
+++ scripts/0_download_ahn4.py
@@ -1,5 +1,6 @@
 import feedparser
 import requests
+import shutil
 
 feed = feedparser.parse('https://service.pdok.nl/rws/ahn/atom/dtm_05m.xml')
 
@@ -7,4 +8,6 @@ print('Items: ' + str(len(feed.entries[0].links)))
 for entry in feed.entries[0].links:
     if(entry.href.endswith('tif')):
         print('Downloading ' + entry.href)
-        requests.get(entry.href)
+        response = requests.get(entry.href, stream=True)
+        with open('dtms/' + entry.href.split('/')[-1], 'wb') as out_file:
+            shutil.copyfileobj(response.raw, out_file)
