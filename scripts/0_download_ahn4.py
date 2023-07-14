fe813e6e9abe841afb90a07a4ef7ce62365df02f
Kamiel Verhelst <kamielv@leda.geodan.nl>
2023-07-14 16:48:33 +0200

    Updated ahn4 download script

--- scripts/0_download_ahn4.py
+++ scripts/0_download_ahn4.py
@@ -1,13 +1,22 @@
 import feedparser
 import requests
 import shutil
+import os
+
+out_dir = 'dtms/'
 
 feed = feedparser.parse('https://service.pdok.nl/rws/ahn/atom/dtm_05m.xml')
+# Extract all links that end with 'tif'
+links = [l for l in feed.entries[0].links if l.href.endswith('tif')]
+print('Items to download: ' + str(len(links)))
 
-print('Items: ' + str(len(feed.entries[0].links)))
-for entry in feed.entries[0].links:
-    if(entry.href.endswith('tif')):
-        print('Downloading ' + entry.href)
-        response = requests.get(entry.href, stream=True)
-        with open('dtms/' + entry.href.split('/')[-1], 'wb') as out_file:
-            shutil.copyfileobj(response.raw, out_file)
+for entry in links:
+    filename = out_dir + entry.href.split('/')[-1]
+    if os.path.exists(filename):
+        print(f'File {filename} already exists. Skipping.')
+        continue
+    print('Downloading ' + entry.href)
+    response = requests.get(entry.href, stream=True)
+    with open(filename, 'wb') as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+
